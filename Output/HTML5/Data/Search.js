76c9ed50fae31a673e2e1b8a514af230d48778f0
morvenn <morvenn@users.noreply.github.com>
2021-05-12 21:05:03 +0200

    first commit

---
+++ Output/HTML5/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_IntroductionTopics/minimalne_wymagania.htm'],s:['0'],p:[['0',2]]});
---
+++ Output/HTML5/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637564488207085599'
+});
